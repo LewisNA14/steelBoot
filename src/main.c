f280d0e29ec8cf3818c04d6ea168ef480c254eb1
Lewis <lewisnich01@outlook.com>
2026-06-03 17:58:45 +0100

    Refactored the LED_Set function to be fewer lines and better big O as well as renaming to closer align with the functionality of the function as well as adding the TIM2 General Purpose Clock, it's not finished and requires further steps to be used.

--- src/main.c
+++ src/main.c
@@ -5,37 +5,36 @@
  * 
  */
 
+/* Includes ==============================================================================*/ 
 #include <stdint.h>
 #include "stm32f334x8.h"
 
-/*========================================================================================*/
-/**
- * @section Pin Registers
- */
-
-/*========================================================================================*/
-
-typedef enum{
-    LED_OFF,
-    LED_ON
+/* Defined Types =========================================================================*/
+typedef enum led_status_e{
+    ON,
+    OFF,
+    LED_ENUM_END
 } led_status_t;
 
-
-led_status_t LED_Blink (led_status_t led_status)
+/* Functions ============================================================================*/
+/* TODO: Creation of a gpio_set function for the calling / handling of all current and future GPIOs*/
+/* void gpio_set()
 {
-    // Toggle the LED GPIO from OFF to ON
-    if (led_status == LED_OFF)
-    {
-        GPIOA->ODR |= (1U << 5);       // GPIO_ODR_5
-        led_status = LED_ON;
-    }
-    if (led_status == LED_ON)
-    {
-        GPIOA->ODR &= ~(1U << 5);       // GPIO_ODR_5
-        led_status = LED_OFF;
-    }
+    led_status_t LED_Set(pin_no);              // Toggling the LED using BSRR (Bit Set Reset Register).
+}
+ */
+void LED_Set()
+{
+    static led_status_t led_status = OFF;     // Default value is OFF
+
+    led_status++;
+    led_status = led_status % LED_ENUM_END;
+
+    uint8_t bsrr_pos = 5 + (16*led_status);
     
-    // TODO: Toggle the LED Based on the SysTick Timer
+    GPIOA->BSRR = (1U << bsrr_pos);        
+    
+    // TODO: Toggle the LED Based on a simple Timer
 }
 
 // TODO: UART / USB Serial Connection Test Function
@@ -50,26 +49,53 @@ void delay(uint32_t count)
     }
 }
 
+/* TODO: Implement Simple Timer using a TIM peripheral */
+/* Timer Function (TIM2) */
+void timer()
+{
+    // enable the TIM2 Peripheral
+    NVIC_EnableIRQ(TIM2_IRQn);
 
+    // shuts down all un-used peripherals
+
+    // enables mentioned / defined peripherals
+
+    // counts up from 0 to 1 second to enable interrupt to switch LED state
+}
+
+
+/**
+ * @brief Main Program Loop
+ */
 __attribute__((noreturn)) void main() 
 {
     led_status_t led_status;
 
-    led_status = LED_OFF;
+    led_status = OFF;
 
-    // 1. Enable Clock for GPIOA EN
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    /* RCC Peripheral Enabling / Initialisation */
+    RCC->AHBENR     |= RCC_AHBENR_GPIOAEN;                      // Enable Clock for GPIOA EN
+    RCC->APB1ENR    |= RCC_APB1ENR_TIM2EN;                      // Enable clock for TIM2 EN
 
-    // 2. Clearing GPIOA Pin 5
-    GPIOA->MODER &= ~(0x3U << GPIO_MODER_MODER5_Pos);
-    
-    // 3. Setting the GPIO to Output Mode
-    GPIOA->MODER |= GPIO_MODER_MODER5_0;
+    /* GPIOA Initialisation */
+    GPIOA->MODER    &= ~(0x3U << GPIO_MODER_MODER5_Pos);       // Clearing GPIOA Pin 5
+    GPIOA->MODER    |= GPIO_MODER_MODER5_0;                    // Setting the GPIO to Output Mode
+
+    /* TIM2 Initialisation */
+    TIM2->DIER      |= TIM_DIER_UIE;                            // Enables Update Interrupt 
+    TIM2->PSC       =  7999;                                    // Clock Frequency reduced to every 1ms
+    TIM2->ARR       =  999;                                     // Update Event every 1s
+
+
+    // Clear the TIM2 Pin 
+    // Set the TIM2 with the prescaler
+
+    /* Main Loop */
     while (1)
     {
         delay(8000000);                     // Clock Speed is 8MHz so 5s is 40,000,000 / 5 cycles per loop = 8,000,000
-        // 4. Toggling the LED using ODR (Output Data Register).
-        led_status = LED_Blink(led_status);
+        // gpio_set();          
+        LED_Set();               
         delay(8000000);                  
         
     }
